2ed5f4a29350b5c1eef600c62e9d506347a1b7dc
Kundan Kumar <kd181197@gmail.com>
2022-08-25 00:13:33 +0530

    navbar added

--- Frontend/tmetric/src/App.js
+++ Frontend/tmetric/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Navbar from './pages/Navbar';
-import Signin from './pages/Signin';
+// import Navbar from './pages/Navbar';
+import Navbar from './components/Navbar'
 import Allroutes from './routes/Allroutes';
 
 function App() {
   return (
     <div className="App">
-        <Navbar />
+       <Navbar/>
        <Allroutes />
     </div>
   );
