437752dac3a2c86b660c8dd9f6bfa185a7a8c985
Kundan Kumar <kd181197@gmail.com>
2022-08-26 00:59:41 +0530

    industry page added

--- Frontend/tmetric/src/App.js
+++ Frontend/tmetric/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import Industry from "./pages/industry/Industry";
 import Allroutes from "./routes/Allroutes";
 
 function App() {
