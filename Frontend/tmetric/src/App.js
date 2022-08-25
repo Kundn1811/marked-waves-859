8437b1f0bb4c2134187ddf28a190d5bba34b0d3d
rituraj776930 <83961346+rituraj776930@users.noreply.github.com>
2022-08-25 16:44:31 +0530

    commit

--- Frontend/tmetric/src/App.js
+++ Frontend/tmetric/src/App.js
@@ -1,26 +1,12 @@
-import './App.css';
-
-import Project from './components/Project';
-import Ts from './components/Ts';
-import CreateProject from './pages/CreateProject';
-import SideBar from './pages/SideBar';
-import Signin from './pages/Signin';
-
-
-
+import './App.css'
 import Navbar from './components/Navbar'
 import Allroutes from './routes/Allroutes';
 
 function App() {
   return (
     <div className="App">
-       {/* <Navbar/>
-       <Allroutes /> */}
-  
-       <SideBar/>
-       <Project/>
-       <CreateProject/>
-
+       <Navbar/>
+       <Allroutes />
     </div>
   );
 }
