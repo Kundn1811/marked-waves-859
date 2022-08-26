d138e912d6bd53de3ee4a6e82304ff2d6e0da604
Suhani1102 <sandhusuhani1102@gmail.com>
2022-08-27 00:16:56 +0530

    Pricing slider

--- Frontend/tmetric/src/App.js
+++ Frontend/tmetric/src/App.js
@@ -1,21 +1,19 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import NoResult from "./components/Task/NoResult";
 import TaskContHead from "./components/Task/TaskContHead";
 
 import Allroutes from "./routes/Allroutes";
 
-
 function App() {
   return (
     <div className="App">
-
-       {/* <Navbar/>
-       <Allroutes /> */}
-       {/* <NoResult /> */}
-       <TaskContHead />
-    
-
+      <Navbar />
+      <Allroutes />
+      <Footer />
+      {/* <NoResult /> */}
+      {/* <TaskContHead /> */}
     </div>
   );
 }
