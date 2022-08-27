20b6e606e97ed7a684e03a3bfb48a1f1cc0a04e1
Rohit gupta <rg15697@gmail.com>
2022-08-27 17:04:22 +0530

    updateBackend

--- Backend/Controlers/workSpace.controller.js
+++ Backend/Controlers/workSpace.controller.js
@@ -4,8 +4,10 @@ const authentication  = require("../Middleware/authentication");
 const UserModel = require("../Modals/user.model");
 const workspace = Router();
 
-workspace.get("/",(req,res)=>{
-    res.send("workspace")
+workspace.get("/",authentication,async(req,res)=>{
+    const {userId} = req.body
+    const workSpaces = await WorkSpaceModel.find({userId})
+    return res.send({workSpaces:workSpaces})
 })
 workspace.post("/create",authentication,async(req,res)=>{
     const {teamSize,companyName,userId} = req.body;
@@ -13,11 +15,12 @@ workspace.post("/create",authentication,async(req,res)=>{
     if(!teamSize) return res.send({message:"Please put team size"})
     const workSpaces = await WorkSpaceModel.find({companyName});
     // return res.send(workSpaces)
-    if(!workSpaces.length)  return res.send({message:"workspace with this company name already exists"})
+    console.log(workSpaces.length)
+    if(workSpaces.length)  return res.send({message:"workspace with this company name already exists"})
     const workSpace = new WorkSpaceModel({
         teamSize,companyName,userId
     })
-    workSpace.save();
+    await workSpace.save();
     const user = await UserModel.findOneAndUpdate({userId},{workSpaceId:workSpace._id})
     if(workSpace) return res.send({message : "Workspace created successfully Successfully.",workSpaceId:workSpace._id}) 
     else return res.send({message : "Please fill all field."})       
